22bcead787e30b60fbd48eecce3b7ddb0c663639
Shriphani Palakodety <shriphanip@gmail.com>
2013-03-15 00:18:59 -0400

    added clojars username

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject warc-clojure "0.1.0-SNAPSHOT"
+(defproject org.clojars.shriphani/warc-clojure "0.1.0-SNAPSHOT"
   :description "Wrapper around the jwat-wart Java Library"
   :url "https://github.com/shriphani/warc-clojure"
   :license {:name "MIT License"
