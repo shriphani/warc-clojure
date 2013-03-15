df0dfb6c5cb89b3011bd0a22d90a14168709a680
Shriphani Palakodety <shriphanip@gmail.com>
2013-03-15 00:15:24 -0400

    changed project url to github

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject warc-clojure "0.1.0-SNAPSHOT"
   :description "Wrapper around the jwat-wart Java Library"
-  :url "http://shriphani.com"
+  :url "https://github.com/shriphani/warc-clojure"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.4.0"]
