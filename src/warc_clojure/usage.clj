ecbd8c7df78ea472707caba0a4eb097118960470
Shriphani Palakodety <shriphanip@gmail.com>
2013-03-29 11:20:23 -0400

    added cmdline viewing support

--- src/warc_clojure/usage.clj
+++ src/warc_clojure/usage.clj
@@ -3,6 +3,33 @@
   (:use [clojure.tools.cli :only (cli)])
   (:gen-class :main true))
 
+(defn print-items
+  [record items-bool-map]
+  (when (:content-length items-bool-map) 
+    (print "Content Length:\t" (:content-length record) "\n"))
+  (when (:date items-bool-map)
+    (print "Date:\t" (:date record) "\n"))
+  (when (:content-type items-bool-map)
+    (print "Content Type:\t" (:content-type record) "\n"))
+  (when (:target-uri items-bool-map)
+    (print "Target Uri:\t" (:target-uri record) "\n"))
+  (when (:warc-type items-bool-map)
+    (print "Warc Type:\t" (:warc-type record)) "\n")
+  (when (:payload items-bool-map)
+    (print "Payload:\n" (slurp (:payload-stream record))))
+  (println "############################################"))
+
+
 (defn -main
   [& args]
-  (map (fn [record] (:content-length record)) (core/get-response-records-seq (core/get-warc-reader (first (second (cli args)))))))
+  (let [[args-vector [warc-gz-filename] banner] 
+  			(cli args ["--content-length" "Prints out the sizes of each file" :flag true]
+  					  ["--content-type" "Prints out the content type" :flag true]
+  					  ["--date" "Prints out the date of download" :flag true]
+  					  ["--target-uri" "Prints out the url of the warc file" :flag true]
+  					  ["--warc-type" "Prints out the warc-type field" :flag true]
+  					  ["--payload" "Prints out the html stream"])]
+
+  		(doseq [record (core/get-response-records-seq 
+							(core/get-warc-reader warc-gz-filename))]
+      		(print-items record args-vector))))
